5ad678fdfe14e2a0891f192e4247837640e122fe
EllaGliksberg <ellaglik@gmail.com>
2026-01-06 12:29:55 +0200

    changed the student details

--- app/src/main/java/com/example/myapplication/NewStudentActivity.kt
+++ app/src/main/java/com/example/myapplication/NewStudentActivity.kt
@@ -15,6 +15,8 @@ class NewStudentActivity : AppCompatActivity() {
 
         val nameEditText: EditText = findViewById(R.id.et_new_student_name)
         val idEditText: EditText = findViewById(R.id.et_new_student_id)
+        val addressEditText: EditText = findViewById(R.id.et_new_student_address)
+        val phoneEditText: EditText = findViewById(R.id.et_new_student_phone)
         val checkedCheckBox: CheckBox = findViewById(R.id.cb_new_student_checked)
         val cancelButton: Button = findViewById(R.id.btn_new_student_cancel)
         val saveButton: Button = findViewById(R.id.btn_new_student_save)
@@ -26,9 +28,11 @@ class NewStudentActivity : AppCompatActivity() {
         saveButton.setOnClickListener {
             val name = nameEditText.text.toString()
             val id = idEditText.text.toString()
+            val address = addressEditText.text.toString()
+            val phone = phoneEditText.text.toString()
             val isChecked = checkedCheckBox.isChecked
 
-            val newStudent = Student(name, id, isChecked, "")
+            val newStudent = Student(name, id, isChecked, "", address, phone)
             StudentsModel.students.add(newStudent)
 
             finish()
--- app/src/main/java/com/example/myapplication/model/StudentsModel.kt
+++ app/src/main/java/com/example/myapplication/model/StudentsModel.kt
@@ -4,8 +4,8 @@ object StudentsModel {
     val students = mutableListOf<Student>()
 
     init {
-        students.add(Student("Ella Gliksberg", "123456789", false, ""))
-        students.add(Student("John Doe", "987654321", true, ""))
-        students.add(Student("Jane Smith", "111222333", false, ""))
+        students.add(Student("Ella Gliksberg", "123456789", false, "", "Ramat Gan", "050-1234567"))
+        students.add(Student("John Doe", "987654321", true, "", "New York", "052-9876543"))
+        students.add(Student("Jane Smith", "111222333", false, "", "London", "054-1112223"))
     }
 }
