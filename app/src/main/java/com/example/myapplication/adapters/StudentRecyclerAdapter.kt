978bb93fd8ca337ddcf52d91f6d5ba9b1225a04f
EllaGliksberg <ellaglik@gmail.com>
2026-01-06 12:35:07 +0200

    added avatar

--- app/src/main/java/com/example/myapplication/adapters/StudentRecyclerAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapters/StudentRecyclerAdapter.kt
@@ -32,6 +32,7 @@ class StudentRecyclerAdapter(private val students: List<Student>) : RecyclerView
         holder.studentName.text = currentStudent.name
         holder.studentId.text = currentStudent.id
         holder.studentCheckBox.isChecked = currentStudent.isChecked
+        holder.studentAvatar.setImageResource(R.mipmap.ic_launcher) // Set a default image
 
         // Listeners
         holder.itemView.setOnClickListener {
